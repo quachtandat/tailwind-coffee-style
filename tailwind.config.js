569af9e29cb5354adf26369fb2c7f0561cf918ee
quachtandat <quachbao18@gmail.com>
2024-08-12 10:55:42 +0700

    add
    add

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-coffee':'#C98F94'
+      }
+    },
+  },
+  plugins: [],
+}
+
